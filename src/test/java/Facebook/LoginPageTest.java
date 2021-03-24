b731c26eeda92bcba73b18375e74f5fe7a46c635
Vikas <vikastripathi1435@gmail.com>
2021-03-24 23:58:43 +0530

    Vikas commited second times in asian time

--- src/test/java/Facebook/LoginPageTest.java
+++ src/test/java/Facebook/LoginPageTest.java
@@ -8,11 +8,13 @@ public class LoginPageTest {
 	public void EnterDetails(){
 		
 		System.out.println("Welcome to facebook");
+		System.out.println("Welcome to Vikas home page");
 		
 	}
 @Test
 public void ClickOnLogin()
 {
 	System.out.println("Looged in facebook.");
+	System.out.println("Looged in Vikas facebook account.");
 }
 }
