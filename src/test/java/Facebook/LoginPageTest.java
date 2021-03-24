882c822d4c2384d6397eb4666a8420d326f87fbb
Vikas <vikastripathi1435@gmail.com>
2021-03-25 00:14:01 +0530

    architect asian development

--- src/test/java/Facebook/LoginPageTest.java
+++ src/test/java/Facebook/LoginPageTest.java
@@ -7,8 +7,17 @@ public class LoginPageTest {
 	@Test
 	public void EnterDetails(){
 		
+
 		System.out.println("Welcome to facebook");
 		System.out.println("Welcome to Vikas home page");
+		System.out.println("Welcome to facebgewweegwook");
+		System.out.println("Welcome to weg home page");
+		
+		System.out.println("Welcome to faceegwwgewegbook");
+		System.out.println("Welcome to VikGegGegas home page");
+		
+		System.out.println("Welcome to EGge");
+		System.out.println("Welcome to Viggegeg");
 		
 	}
 @Test
@@ -16,5 +25,28 @@ public void ClickOnLogin()
 {
 	System.out.println("Looged in facebook.");
 	System.out.println("Looged in Vikas facebook account.");
+	System.out.println("Looged in 14.");
+	System.out.println("Looged in Vi2555kas facebook account.");
+	System.out.println("Looged FFn Vikas face544521book account.");
+}
+@Test
+public void ClickOnogin()
+{
+	System.out.println("Looged in facebook.");
+	System.out.println("Loogedfw in Vikas facebook account.");
+	System.out.println("Looged in 14.");
+	System.out.println("Looged fin Vi2555kas facebook account.");
+	System.out.println("Looged in facebo5555ok.");
+	System.out.println("Looged inw Vikas face544521book account.");
+}
+@Test
+public void ClickOnLoin()
+{
+	System.out.println("Looged in facebook.");
+	System.out.println("Looged in Vikas facebook account.");
+	System.out.println("Looged in 14.");
+	System.out.println("Looged wfin Vi2555kas facebook account.");
+	System.out.println("Looged in facebo5555ok.");
+	System.out.println("Loogedffw in Vikas face544521book account.");
 }
 }
